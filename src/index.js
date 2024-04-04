2323e659c83eaa562082b1a814d7360b91998095
AnujM1001 <anujmathur24@gmail.com>
2024-04-04 23:56:23 +0530

    API error and response hanlders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 // require('dotenv').config()
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 });
 
 
-connectDB();
+connectDB().then(() => {
+    app.on("error", () => {
+        console.log("app crashed", app)
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Server started to listen at:" , process.env.PORT || 8000)
+    })
+}).catch((err) => {
+    console.log("MONGODB connection fail", err)
+});
